446f097937deab8daad21413535b1925d479f060
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-11-10 09:21:47 -0700

    basic eval calculator

--- client/src/main/views/calculator/Calculator.js
+++ client/src/main/views/calculator/Calculator.js
@@ -5,13 +5,26 @@ import CalculatorComponent from "./CalculatorComponent";
 class Calculator extends React.Component {
     constructor() {
         super();
+        this.concatOutput = this.concatOutput.bind(this);
+        this.evaluateOutput = this.evaluateOutput.bind(this);
+    }
+
+    concatOutput(event) {
+        document.getElementById("output").innerHTML += event.target.value;
+    }
+
+    evaluateOutput(e) {
+        let total = document.getElementById("output").innerHTML;
+        document.getElementById("output").innerHTML = eval(total);
     }
 
     render() {
         return (
             <div>
                 <Navbar/>
-                <CalculatorComponent/>
+                <CalculatorComponent
+                    concat={this.concatOutput}
+                    evaluate={this.evaluateOutput}/>
             </div>
         )
     }
--- client/src/main/views/calculator/CalculatorComponent.js
+++ client/src/main/views/calculator/CalculatorComponent.js
@@ -7,7 +7,7 @@ export default function CalculatorComponent(props) {
         padding: "25px 0 25px 0",
         width: "300px",
         margin: "auto",
-        border: "2px solid yellow",
+        border: "2px solid green",
         marginTop: "55px"
     }
 
@@ -15,7 +15,13 @@ export default function CalculatorComponent(props) {
         width: "250px",
         margin: "auto",
         height: "100px",
-        border: "2px solid yellow",
+        border: "2px solid green",
+        padding: "15px",
+        display: "flex",
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        fontSize: "2em",
+        color: "green"
     }
 
     const rowStyles = {
@@ -28,41 +34,39 @@ export default function CalculatorComponent(props) {
     const buttonStyles = {
         width: "50px",
         height: "50px",
-        border: "2px solid yellow",
-
+        border: "2px solid green",
+        backgroundColor: "transparent",
+        color: "green",
+        fontSize: "3em",
+        outline: "none"
     }
+
     return (
         <div style={calcStyles}>
-            <div style={outputStyles}></div>
+            <p id="output" style={outputStyles}></p>
             <div style={rowStyles}>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
+                <input onClick={props.concat} type="button" name="" value="7" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="8" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="9" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="/" style={buttonStyles}/>
             </div>
             <div style={rowStyles}>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
+                <input onClick={props.concat} type="button" name="" value="4" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="5" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="6" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="*" style={buttonStyles}/>
             </div>
             <div style={rowStyles}>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
+                <input onClick={props.concat} type="button" name="" value="1" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="2" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="3" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="-" style={buttonStyles}/>
             </div>
             <div style={rowStyles}>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-            </div>
-            <div style={rowStyles}>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
-                <div style={buttonStyles}></div>
+                <input onClick={props.concat} type="button" name="" value="0" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="." style={buttonStyles}/>
+                <input onClick={props.evaluate} type="button" name="" value="=" style={buttonStyles}/>
+                <input onClick={props.concat} type="button" name="" value="+" style={buttonStyles}/>
             </div>
         </div>
     )
