bc3d091ea49a45cad7408f8c4c71cf86494177d7
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-11-29 20:25:15 -0500

    add logout

--- client/src/main/Navbar.js
+++ client/src/main/Navbar.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "./views/login/redux/login";
 
-export default function Navbar() {
+function Navbar(props) {
     const containerStyle = {
         display: "flex",
         justifyContent: "space-around",
         alignItems: "center",
         height: "50px",
-        borderBottom: "2px solid white"
+        borderBottom: "2px solid white",
     }
 
     const linkStyles = {
         color: "white",
         textDecoration: "none",
-        fontSize: "1.5em"
+        fontSize: "1.5em",
+        cursor: "pointer",
     }
 
     return (
@@ -21,6 +24,9 @@ export default function Navbar() {
             <Link to="/calculator" style={linkStyles}>CALCULATOR</Link>
             <Link to="/journal" style={linkStyles}>JOURNAL</Link>
             <Link to="/shitlist" style={linkStyles}>SHIT LIST</Link>
+            <h1 onClick={props.logout} style={linkStyles}>LOGOUT</h1>
         </div>
     )
 }
+
+export default connect(null, {logout})(Navbar);
