f7bcd970e61ea25cd15f347b0040861e0d53371a
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-12-04 11:51:44 -0500

    update secret

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 10100,
-    secret: process.env.SECRET || "fish umbrella dogs octo",
+    secret: process.env.SECRET || "fish umbrella dogs cactus",
     db: process.env.DB || "mongodb://localhost/diary",
 }
